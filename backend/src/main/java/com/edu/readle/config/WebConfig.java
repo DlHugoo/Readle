af447af57f51c46af7e6c2d03b47f62c2800d78e
DlHugoo <hugo.davelawrence@gmail.com>
2025-10-20 23:01:07 +0800

    Remove Bean in WebConfig

--- backend/src/main/java/com/edu/readle/config/WebConfig.java
+++ backend/src/main/java/com/edu/readle/config/WebConfig.java
@@ -1,9 +1,7 @@
 package com.edu.readle.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
@@ -17,11 +15,5 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     // CORS is now configured in SecurityConfig.corsConfigurationSource
-
-    // RestTemplate bean for making external API calls
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 
 }
